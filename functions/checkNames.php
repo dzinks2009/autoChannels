600fa9712256a7aaf5d4aa8840cede23af035f50
dzinks2009 <dzinks2009@gmail.com>
2018-05-12 00:23:51 -0400

    Update checkNames.php

--- functions/checkNames.php
+++ functions/checkNames.php
@@ -9,26 +9,26 @@ class checkNames{
 				$groups = $ts->serverGroupList()["data"];
 				foreach($groups as $group) {
 					if($group['sgid'] == $channel["group_id"]) {
-						$group = $group;
+						$cGroup = $group;
 					}
 				}
 
-				if(strcmp($channel["group_name"], $group['name'])){
+				if(strcmp($channel["group_name"], $cGroup['name'])){
 					$statement = $pdo->prepare("UPDATE `".$zone."Channels` SET `group_name`=? WHERE id=?");
-					$statement->execute(array($group["name"], $channel["id"]));
+					$statement->execute(array($cGroup["name"], $channel["id"]));
 				}
 
 				foreach($zConfig["channelsToCreate"] as $id => $channelsToEdit){
 					if(isset($channelsToEdit["dbName"]) && isset($channelsToEdit["correctName"])){
 						$channelInfo = $ts->channelInfo($channel[$channelsToEdit["dbName"]])["data"];
-						if($channelInfo["channel_name"] !== str_replace(array("[CH_NUMER]", "[NAME]"), array($channel["num"], $channel["group_name"]), $channelsToEdit["name"])){
+						if($channelInfo["channel_name"] !== str_replace(array("[CH_NUMER]", "[NAME]"), array($channel["num"], $cGroup['name']), $channelsToEdit["name"])){
 							$ts->channelEdit($channel[$channelsToEdit["dbName"]],array('channel_name' => str_replace(array("[CH_NUMER]", "[NAME]"), array($channel["num"], $channel["group_name"]), $channelsToEdit["name"])));
 						}
 					}
 
 					if(isset($channelsToEdit["subChannels"]["dbName"]) && isset($channelsToEdit["subChannels"]["correctName"])){
 						$channelInfo = $ts->channelInfo($channel[$channelsToEdit["subChannels"]["dbName"]])["data"];
-						if($channelInfo["channel_name"] !== str_replace(array("[CH_NUMER]", "[NAME]"), array($channel["num"], $channel["group_name"]), $channelsToEdit["subChannels"]["name"])){
+						if($channelInfo["channel_name"] !== str_replace(array("[CH_NUMER]", "[NAME]"), array($channel["num"], $cGroup['name']), $channelsToEdit["subChannels"]["name"])){
 							$ts->channelEdit($channel[$channelsToEdit["subChannels"]["dbName"]],array('channel_name' => str_replace(array("[CH_NUMER]", "[NAME]"), array($channel["num"], $channel["group_name"]), $channelsToEdit["subChannels"]["name"])));
 						}
 					}
