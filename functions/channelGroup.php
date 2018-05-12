6c04b99d5bae95428321421077bc16f95fa34151
dzinks2009 <dzinks2009@gmail.com>
2018-05-12 10:19:13 -0400

    Update channelGroup.php

--- functions/channelGroup.php
+++ functions/channelGroup.php
@@ -14,10 +14,15 @@ class channelGroup{
            						$ts->serverGroupDeleteClient($channel["group_id"], $person['client_database_id']);
 							$ts->clientKick($person["clid"], "channel", "Wlasnie odszedles z klanu!");
 							$ts->clientPoke($person["clid"], "Wlasnie odszedles z klanu!");
+
+                       					$ts->setClientChannelGroup(0, $channel[$zConfig["mainChannelTable"]], $person["client_database_id"]); 
 						}else{
            						$ts->serverGroupAddClient($channel["group_id"], $person['client_database_id']);
 							$ts->clientMove($person["clid"], $channel[$zConfig["mainChannelTable"]]);
 							$ts->clientPoke($person["clid"], "Wlasnie doszedles do klanu!");
+
+                       					$ts->setClientChannelGroup($zConfig["verifyChannelGroup"], $channel[$zConfig["mainChannelTable"]], $person["client_database_id"]); 
+
 						}
 					}
 				}
